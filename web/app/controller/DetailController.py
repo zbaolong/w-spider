50334bc473c7558fef5bae2bbd3c02cafa64247a
wangtao <965929275@qq.com>
2018-05-27 13:15:19 +0800

    shangai

--- web/app/controller/DetailController.py
+++ web/app/controller/DetailController.py
@@ -1,5 +1,11 @@
 # encoding:utf-8
 from flask_restful import Resource
+from app.models.Detail import Detail
+from sqlalchemy import and_
+from flask_restful import Resource, reqparse
+from app import db
+from flask import request
+
 
 class DetailController(Resource):
 
@@ -11,7 +17,13 @@ class DetailController(Resource):
         :param pnNumber:
         :return:
         """
-        pass
+        detail = Detail.query.filter(and_(uuid == uuid,\
+                                          Detail.item_number == itemNumber,\
+                                          Detail.paragraph_number == pnNumber)).first()
+        detail.paragraph_content = request.json.get('paragraph_content')
+        db.session.commit()
+        return 'put success'
+
 
     def delete(self,uuid,itemNumber,pnNumber):
         """
@@ -21,4 +33,9 @@ class DetailController(Resource):
         :param pnNumber:
         :return:
         """
-        pass
+        detail = Detail.query.filter(and_(uuid == uuid, \
+                                          Detail.item_number == itemNumber, \
+                                          Detail.paragraph_number == pnNumber)).first()
+        db.session.delete(detail)
+        db.session.commit()
+        return 'detele success'
